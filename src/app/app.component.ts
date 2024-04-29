250b6cdafeec993561b3fdd8f1868e559a417d51
JTorres <ta_jorge_luis@hotmail.com>
2024-04-28 20:30:09 -0500

    Avance 28.04.2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ecommerce-angular-app';
+
+  constructor(private fs: AngularFirestore) {}
+
+  ngOnInit() {
+    this.fs
+      .collection('test')
+      .snapshotChanges()
+      .subscribe((personas) => {
+        console.log(personas.map(x => x.payload.doc.data()))
+      });
+  }
 }
